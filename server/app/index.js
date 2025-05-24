8c91a5daf80d6b7f7f6393e5ed0b6f0bf955c388
hemel17 <redewanmahmud@gmail.com>
2025-05-24 11:58:43 +0600

    render settings added

--- server/app/index.js
+++ server/app/index.js
@@ -4,6 +4,7 @@ import express from "express";
 import middleware from "./middleware.js";
 import router from "../routes/index.js";
 import { notFoundHandler, errorHandler } from "./error.js";
+import sendGetReq from "../automation/sendGetReq.js";
 const app = express();
 
 // * middlewares
@@ -11,6 +12,9 @@ app.use(middleware);
 
 // * routes
 app.use(router);
+
+// * automation
+sendGetReq();
 
 // * error handlers
 app.use(notFoundHandler);
